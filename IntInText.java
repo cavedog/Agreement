33dda224d8aa81bd16aa315b0d98df0c71ce246b
cavedog <AVakaruk@gmail.com>
2014-09-03 16:14:38 +0300

    interface addings

---
+++ IntInText.java
@@ -0,0 +1,138 @@
+package AgreementMaker;
+
+	import java.lang.reflect.Field;
+
+	public class IntInText {
+	public final static String x1="один";
+	public final static String xx1="одна";
+
+	public final static String x2="два";
+	public final static String xx2="двi";
+
+
+	public final static String x3="три";
+	public final static String x4="чотири";
+	public final static String x5="п'€ть";
+	public final static String x6="шiсть";
+	public final static String x7="сiм";
+	public final static String x8="вiсiм";
+	public final static String x9="дев'€ть";
+	public final static String x10="дес€ть";
+
+	public final static String x11="одинадц€ть";
+	public final static String x12="дванадц€ть";
+	public final static String x13="тринадц€ть";
+	public final static String x14="чотирнадц€ть";
+	public final static String x15="п'€тнадц€ть";
+	public final static String x16="шiстнадц€ть";
+	public final static String x17="сiмнадц€ть";
+	public final static String x18="вiсiмнадц€ть";
+	public final static String x19="дев'€тнадц€ть";
+
+	public final static String x20="двадц€ть";
+	public final static String x30="тридц€ть";
+	public final static String x40="сорок";
+	public final static String x50="п'€тдес€т";
+	public final static String x60="шiстдес€т";
+	public final static String x70="сiмдес€т";
+	public final static String x80="вiсiмдес€т";
+	public final static String x90="дев'€носто";
+
+	public final static String x100="сто";
+	public final static String x200="двiстi";
+	public final static String x300="триста";
+	public final static String x400="чотириста";
+	public final static String x500="п'€тсот";
+	public final static String x600="шiстсот";
+	public final static String x700="сiмсот";
+	public final static String x800="вiсiмсот";
+	public final static String x900="дев'€тсот";
+
+	public final static String x1000="тис€ча";
+	public final static String xx1000="тис€чi";
+	public final static String xхx1000="тис€ч";
+
+
+
+	public static String convert(int x){
+	String no=String.valueOf(x);
+	String res="";
+	Field fild;
+	try{
+	Class clas = IntInText.class;
+
+	if(no.charAt(no.length()-1)!='0'){
+	fild = IntInText.class.getField("x"+no.charAt(no.length()-1));
+	res=" "+fild.get(clas).toString();
+	}
+
+	if(no.length()-2>-1 && no.charAt(no.length()-2)=='1'){
+	fild = IntInText.class.getField("x1"+no.charAt(no.length()-1));
+	res=" "+fild.get(clas).toString();
+	}
+	//if(x>19 && no.charAt(no.length()-1)!='0'){
+	if(x>19){
+	fild = IntInText.class.getField("x"+no.charAt(no.length()-2)+"0");
+	res=" "+fild.get(clas).toString()+res;
+	}
+
+	if(x>99 && no.charAt(no.length()-3)!='0'){
+	fild = IntInText.class.getField("x"+no.charAt(no.length()-3)+"00");
+	res=" "+fild.get(clas).toString()+res;
+	}
+
+
+	if(x>999 && no.length()-5==-1){	
+	String isRes=no.charAt(no.length()-4)+"";
+
+	if(isRes.equals("1")){
+	fild = IntInText.class.getField("xx"+no.charAt(no.length()-4));
+	res=" "+fild.get(clas).toString()+" "+x1000+res;
+	}
+	if(isRes.equals("2")){
+	fild = IntInText.class.getField("xx"+no.charAt(no.length()-4));
+	res=" "+fild.get(clas).toString()+" "+xx1000+res;
+	}	
+	if(isRes.equals("3")|| isRes.equals("4")){
+	fild = IntInText.class.getField("x"+no.charAt(no.length()-4));
+	res=" "+fild.get(clas).toString()+" "+xx1000+res;
+	}
+	if(no.charAt(no.length()-4)!='0' && !isRes.equals("1")&&!isRes.equals("2") && !isRes.equals("3") && !isRes.equals("4")){
+	fild = IntInText.class.getField("x"+no.charAt(no.length()-4));
+	res=" "+fild.get(clas).toString()+" "+xхx1000+res;
+	}
+	}	
+
+
+
+	if(x>9999 && no.charAt(no.length()-5)=='1'){
+	fild = IntInText.class.getField("x1"+no.charAt(no.length()-4));
+	res=" "+fild.get(clas).toString()+" "+xхx1000+res;
+	}
+
+
+
+	if(x>19999){
+
+	fild = IntInText.class.getField("x"+no.charAt(no.length()-5)+"0");
+	if(no.charAt(no.length()-4)=='0')
+	res=" "+fild.get(clas).toString()+" "+xхx1000+res;
+	else
+	res=" "+fild.get(clas).toString()+res;	
+
+	}	
+
+	}catch(Exception e){
+	e.printStackTrace();
+	}
+
+	return res;
+	}
+
+	public static void main(String [] argm){
+	int x = 1220;
+	String text = convert(x);
+	System.out.println("text="+text);	
+	}
+
+	}
--- mainFrame.java
+++ mainFrame.java
@@ -31,11 +31,18 @@ public class mainFrame extends JFrame {
 	JButton 	clearButton = new JButton("Clear");
 	JButton 	SettingsButton 	= new JButton("Settings");
 	JButton 	ExitButton = new JButton("Exit");
+	JButton 	test = new JButton("test");
 	
 	//Labels
 	
 	JLabel	lbDateOfAgreement = new JLabel("Date of Agreement:");
+	JLabel	lbNameOfCourse = new JLabel("Course :");
+	JLabel	lbtrainingPeriod = new JLabel("Training period (month) :");
 	JLabel	lbDateOfStart = new JLabel("Start education :");
+	JLabel	lbcostTraining = new JLabel("Total Cost of Course :");
+	JLabel	lbcostTrainingP1 = new JLabel("Cost of Course (part 1) :");
+	JLabel	lbcostTrainingP2 = new JLabel("Cost of Course (part 2) :");
+	JLabel	lbcostTrainingP3 = new JLabel("Cost of Course (part 3) :");
 	JLabel	lbSurname = new JLabel("Surname:");
 	JLabel	lbName = new JLabel("Name:");
 	JLabel	lbSecondName = new JLabel("Second name:");
@@ -49,7 +56,13 @@ public class mainFrame extends JFrame {
 	
 	// Describing TEXT forms 	
 	JTextField	dateOfAgreement = new JTextField(50);
+	JTextField	nameOfCourse = new JTextField(50);
 	JTextField	dateOfStart = new JTextField(50);
+	JTextField	trainingPeriod = new JTextField(50);
+	JTextField	costTraining = new JTextField(50);
+	JTextField	costTrainingP1 = new JTextField("---",50);
+	JTextField	costTrainingP2 = new JTextField("---",50);
+	JTextField	costTrainingP3 = new JTextField("---",50);
 	JTextField	surname = new JTextField(50);
 	JTextField	name = new JTextField(50);
 	JTextField	secondName = new JTextField(50);
@@ -58,6 +71,8 @@ public class mainFrame extends JFrame {
 	JTextField	inn = new JTextField(50);
 	JTextField	adress = new JTextField(50);
 	JTextField	eMail = new JTextField(50);
+	
+	
 	
 		// Describing Panels
 	JPanel 		toolPanel 	= new JPanel();
@@ -73,13 +88,13 @@ public class mainFrame extends JFrame {
 		super();
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setTitle("Agreement Maker");
-		setBounds(100, 100, 850, 370);
+		setBounds(100, 100, 850, 420);
 		//setResizable(false);
 		
 		toolPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1));
-		labelsPanel.setLayout(new GridLayout(10,1));
-		datePanel.setLayout(new GridLayout(10,1));
-		enterPanel.setLayout(new GridLayout(10,1));
+		labelsPanel.setLayout(new GridLayout(16,1));
+		datePanel.setLayout(new GridLayout(16,1));
+		enterPanel.setLayout(new GridLayout(16,1));
 		
 		add(enterPanel, BorderLayout.CENTER);
 		add(toolPanel, BorderLayout.NORTH);
@@ -112,7 +127,43 @@ public class mainFrame extends JFrame {
 		        }
 		       };
 		  dateStartPicker.addActionListener (a2); 
-				  
+		
+		// Course Name ComboBox
+		  String[] nameCourseAr  = { "QA Manual", "QA Automation", "Java", "Front-end", ".NET" };
+			JComboBox nameCourse = new JComboBox(nameCourseAr);
+			nameCourse.setEditable(true);
+			nameCourse.setSelectedIndex(1);
+			nameCourse.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					try {
+						JComboBox cb = (JComboBox)e.getSource();
+				        String nC = (String)cb.getSelectedItem();
+				        nameOfCourse.setText(nC);
+				       	} catch (Exception d) {
+						d.printStackTrace();
+					}
+				}
+			});
+		  		  
+		  // Training Period ComboBox
+		  String[] trainingPeriodAr  = { "1", "2", "3", "4", "5", "6" };
+			JComboBox trPeriod = new JComboBox(trainingPeriodAr);
+			trPeriod.setEditable(true);
+			trPeriod.setSelectedIndex(2);
+			trPeriod.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					try {
+						JComboBox cb = (JComboBox)e.getSource();
+				        String tP = (String)cb.getSelectedItem();
+				        trainingPeriod.setText(tP);
+				       	} catch (Exception d) {
+						d.printStackTrace();
+					}
+				}
+			});
+			  
+		  
+		  
 				  
 		toolPanel.add(addAgreementButton);
 		toolPanel.add(viewRegisterButton);
@@ -121,8 +172,15 @@ public class mainFrame extends JFrame {
 		toolPanel.add(ExitButton);
 		
 		
+		
+		labelsPanel.add(lbNameOfCourse);
 		labelsPanel.add(lbDateOfAgreement);
 		labelsPanel.add(lbDateOfStart);
+		labelsPanel.add(lbtrainingPeriod);
+		labelsPanel.add(lbcostTraining);
+		labelsPanel.add(lbcostTrainingP1);
+		labelsPanel.add(lbcostTrainingP2);
+		labelsPanel.add(lbcostTrainingP3);
 		labelsPanel.add(lbSurname);
 		labelsPanel.add(lbName);
 		labelsPanel.add(lbSecondName);
@@ -132,8 +190,15 @@ public class mainFrame extends JFrame {
 		labelsPanel.add(lbAdress);
 		labelsPanel.add(lbEMail);
 		
+		
+		enterPanel.add(nameOfCourse);
 		enterPanel.add(dateOfAgreement);
 		enterPanel.add(dateOfStart);
+		enterPanel.add(trainingPeriod);
+		enterPanel.add(costTraining);
+		enterPanel.add(costTrainingP1);
+		enterPanel.add(costTrainingP2);
+		enterPanel.add(costTrainingP3);
 		enterPanel.add(surname);
 		enterPanel.add(name);
 		enterPanel.add(secondName);
@@ -143,8 +208,11 @@ public class mainFrame extends JFrame {
 		enterPanel.add(adress);
 		enterPanel.add(eMail);
 	
+		datePanel.add (nameCourse);
 		datePanel.add (dateAgrPicker);
 		datePanel.add (dateStartPicker);
+		datePanel.add (trPeriod);
+		datePanel.add (test);
 		
 		donePanel.add(formAgreementButton);
 		donePanel.add(clearButton);
@@ -165,9 +233,6 @@ public class mainFrame extends JFrame {
 			}
 		});
 		
-		
-		  
-				
 		ExitButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
                 System.exit(0);
@@ -178,7 +243,9 @@ public class mainFrame extends JFrame {
 		    
 		   public void actionPerformed(ActionEvent e) {
 			dateOfAgreement.setText(null);
+			nameOfCourse.setText(null);
 			dateOfStart.setText(null);
+			trainingPeriod.setText(null);
 			surname.setText(null);
 			name.setText(null);
 			secondName.setText(null);
@@ -190,8 +257,20 @@ public class mainFrame extends JFrame {
 		    }
 		});
 		
-		
-		
+			
+		test.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int Ct=Integer.parseInt (costTraining.getText());
+				int CtP1= Integer.parseInt (costTraining.getText());
+				int CtP2= Integer.parseInt (costTraining.getText());
+				int CtP3= Integer.parseInt (costTraining.getText());
+				
+				String CtToText= IntInText.convert(Ct);  
+				System.out.println(CtToText);
+			}
+		});
+				
+				
 	}
 
 }
